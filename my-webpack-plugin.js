86b0a7a3609c236e94be2c620eaea9c50dbabee5
wanghairong-i <3189578410@qq.com>
2019-06-21 13:12:13 +0800

    up

--- my-webpack-plugin.js
+++ my-webpack-plugin.js
@@ -6,8 +6,6 @@ module.exports = class MyWebpackPlugin {
     this.options = options;
   }
   apply(compiler) {
-    console.log('start-', pluginName, '\n');
-    console.log('options-', compiler.options, '\n');
     compiler.hooks.run.tap(pluginName, compilation => {
       console.log('compilation \n', compilation);
       
