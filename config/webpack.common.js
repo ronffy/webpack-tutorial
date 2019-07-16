e47c4df14f2bf10f1f49b1ebe9a21d8a8b7e6984
wanghairong-i <3189578410@qq.com>
2019-07-16 14:30:08 +0800

    add writeVersion 版本文件夹

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { version } = require('./package.json');
+const { version } = require('../package.json');
 
 const cwd = process.cwd();
 const PATHS = {
