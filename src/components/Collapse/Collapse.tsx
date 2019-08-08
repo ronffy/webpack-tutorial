00b0882a0631c296ce45d9f4fb40e6173148b809
wanghairong-i <3189578410@qq.com>
2019-08-08 10:26:43 +0800

    up CollapseItem

--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import CollapseItem, { Props as ItemBaseProps } from './CollapseItem';
+import React, { ReactNode } from 'react';
+import CollapseItem, { Props as CollapseItemProps } from './CollapseItem';
 
-type ItemProps = ItemBaseProps & {
-  id: string | number;
+interface Item {
+  id: string | number,
+  title: CollapseItemProps['title'],
+  content: ReactNode,
 }
 
 interface Props {
-  menu: ItemProps[]
+  menu: Item[]
 }
 
 const Collapse = ({ menu }: Props) => (
   <>
     {menu.map(({ id, title, content }) => (
       <React.Fragment key={id}>
-        <CollapseItem title={title} content={content} />
+        <CollapseItem title={title}>{content}</CollapseItem>
       </React.Fragment>
     ))}
   </>
--- src/components/Collapse/CollapseItem.tsx
+++ src/components/Collapse/CollapseItem.tsx
@@ -1,19 +1,18 @@
-import React, { SFC, ReactNode } from 'react';
+import React, { SFC } from 'react';
 import Toggleable from '../Toggleable';
 
 export type Props = {
   title: string,
-  content: ReactNode,
 }
 
-const CollapseItem: SFC<Props> = ({ title, content }) => (
+const CollapseItem: SFC<Props> = ({ title, children }) => (
   <Toggleable>
     {({ show, toggle }) => (
       <div style={{ border: '1px solid #ccc' }}>
         <h3 onClick={toggle} style={{ padding: '5px', background: '#eee' }}>{title}</h3>
         {
           show
-            ? content
+            ? children
             : null
         }
       </div>
